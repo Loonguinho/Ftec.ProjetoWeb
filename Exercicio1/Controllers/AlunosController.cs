ce805ca28d44a54483165751d0b29f00bac69120
Lucas Alberto Longo <lucaslongo2000@gmail.com>
2021-08-18 22:05:22 -0300

    Exercício de fixação de Projeto WEB
    primeiro exercício

---
+++ Exercicio1/Controllers/AlunosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ftec.ProjetoWeb.Excercicio1.Controllers
+{
+    public class AlunosController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Listar() => View();
+
+        public IActionResult Gravar() => View();
+
+        public IActionResult Excluir() => View();
+
+        public IActionResult Procurar() => View();
+    }
+}
