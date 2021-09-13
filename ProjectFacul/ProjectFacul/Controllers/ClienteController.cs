38e729d59adb7033ac30beca12e3092263acc56e
VarRgas <89429606+VarRgas@users.noreply.github.com>
2021-09-13 08:21:27 -0300

    ProjectPragaFacul
    Código do projeto da praga com layout baixado da net.

---
+++ ProjectFacul/ProjectFacul/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectFacul.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectFacul/ProjectFacul/MockFactory/MockFactory.cs
@@ -0,0 +1,44 @@
+﻿using ProjectFacul.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectFacul.MockFactory
+{
+    public class MockFactory
+    {
+        public static List<ProdutoModel> GerarListaProdutos(int quantidadeElementos)
+        {
+            List<ProdutoModel> produtos = new List<ProdutoModel>();
+
+            for(int i = 0; i < quantidadeElementos; i++)
+            {
+                produtos.Add(new ProdutoModel()
+                {
+                    Id = Guid.NewGuid(),
+                    CategoriaId = Guid.NewGuid(),
+                    Nome = "Nome do produto",
+                    Quantidade = 10,
+                    UnidadeMedida = "PC"
+                });
+            }
+
+            return produtos;
+        }
+
+        public static List<CategoriaProdutoModel> GerarListaCategoriaProduto(int quantidadeElementos)
+        {
+            List<CategoriaProdutoModel> categorias = new List<CategoriaProdutoModel>();
+
+            for(int i = 0; i < quantidadeElementos; i++)
+            {
+                categorias.Add(new CategoriaProdutoModel()
+                {
+                    Descricao = "Descricao categoria",
+                    Id = Guid.NewGuid()
+                });
+            }
+
+            return categorias;
+        }
+    }
+}
