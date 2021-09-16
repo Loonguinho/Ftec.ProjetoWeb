f622b32ce04cb920deed2554629fa70668f7c689
VarRgas <89429606+VarRgas@users.noreply.github.com>
2021-09-16 10:20:22 -0300

    Changes

--- ProjectFacul/ProjectFacul/Startup.cs
+++ ProjectFacul/ProjectFacul/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjectFacul.Filters;
 
 namespace ProjectFacul
 {
@@ -19,6 +20,11 @@ namespace ProjectFacul
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddMvc(opt =>
+            {
+                opt.Filters.Add(new CustomActionFilter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
