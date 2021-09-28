772e48447ede6f2f6c14a98dcea7bb9a1de9f525
Lucas Alberto Longo <lucaslongo2000@gmail.com>
2021-09-27 21:44:40 -0300

    Atualização de dados
    adicionada algumas referencias para paginas de login, cadastro e recuperar senha

---
+++ ProjectFacul/ProjectFacul/Controllers/CadastroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectFacul.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
