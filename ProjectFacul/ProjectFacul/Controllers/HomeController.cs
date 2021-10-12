037060fde0d6c64c727043abf4986d1f1e1fabbf
Lucas Alberto Longo <lucaslongo2000@gmail.com>
2021-10-12 17:16:40 -0300

    Novas mudanças
    O site agora começa lhe pedindo login
    Algumas alterações adversas

--- ProjectFacul/ProjectFacul/Controllers/HomeController.cs
+++ ProjectFacul/ProjectFacul/Controllers/HomeController.cs
@@ -29,5 +29,10 @@ namespace ProjectFacul.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
     }
 }
